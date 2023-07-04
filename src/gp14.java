47c5390873735027e3de73323cf161545717b35d
peng <gpqhl0071@126.com>
2023-07-04 16:28:29 +0800

    add leetcode 14

---
+++ src/gp14.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 编写一个函数来查找字符串数组中的最长公共前缀。
+ */
+public class gp14 {
+
+  public String longestCommonPrefix(String[] strs) {
+
+    String prefixStr = strs[0];
+
+    for (int i = 0; i < strs.length; i++) {
+      while (strs[i].indexOf(prefixStr) != 0) {
+        prefixStr = prefixStr.substring(0, prefixStr.length() - 1);
+        if (prefixStr.isEmpty()) return "";
+      }
+    }
+
+    return prefixStr;
+  }
+
+}
